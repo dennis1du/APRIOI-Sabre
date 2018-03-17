5ed47a5c71684de84918c23e908d3f93e99db8cc
dennis1du <dujianwei@utexas.edu>
2018-03-16 22:59:37 -0500

    first commit

---
+++ Matrix_Data.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+
+import xlrd
+
+Data_File = xlrd.open_workbook("Matrix_data_sample.xlsx")
+
+'''Sheets loaded'''
+Main_Sheet = Data_File.sheet_by_index(0)
+Overlap = Data_File.sheet_by_index(1)
+Day_Off = Data_File.sheet_by_index(2)
+Pairing_Layover = Data_File.sheet_by_index(3)
+CP1 = Data_File.sheet_by_index(5)
+CP2 = Data_File.sheet_by_index(6)
+
+'''global data reading'''
+# the number of crew memebers/pairings/days/layovers
+m = CP1.nrows - 1
+n = Main_Sheet.nrows - 2
+dn = Day_Off.ncols - 1
+ln = Pairing_Layover.ncols - 1
+
+# LN_j: the number of legs of jth pairing
+LN_j = [[]]
+for j in range(2,n+2):
+    LN_j.append(int(Main_Sheet.cell_value(j,4)))
+
+# LD_j: the number of days of jth pairing   
+LD_j = [[]]
+for j in range(2,n+2):
+    LD_j.append(int(Main_Sheet.cell_value(j,3)))
+
+# LH_j: the number of hours of jth pairing
+LH_j = [[]]
+for j in range(2,n+2):
+    LH_j.append(int(Main_Sheet.cell_value(j,7)))
+
+# BH_j: the block hours of jth pairing
+BH_j = [[]]
+for j in range(2,n+2):
+    BH_j.append(int(Main_Sheet.cell_value(j,8)))
+
+#O_gh: the gth pairing and the hth pairing are overlapped/not
+O_gh = [[]]
+for g in range(1,n+1):
+    row = [[]]
+    for h in range(1,n+1):
+        row.append(int(Overlap.cell_value(g,h)))
+    O_gh.append(row)
+
+# DO_jd: jth pairing has dth day to work/off
+DO_jd = [[]]
+for j in range(1,n+1):
+    row = [[]]
+    for d in range(1,dn+1):
+        row.append(int(Day_Off.cell_value(j,d)))
+    DO_jd.append(row)
+
+# LO_jl: jth pairing has lth lay-over
+LO_jl = [[]]
+for j in range(1,n+1):
+    row = [[]]
+    for l in range(1,ln+1):
+        row.append(int(Pairing_Layover.cell_value(j,l)))
+    LO_jl.append(row)
+
+L_j = [[]]
+for j in range(1,n+1):
+    row = [[]]
+    s = 0
+    for l in range(1,ln+1):
+        s += int(Pairing_Layover.cell_value(j,l))
+    if s>=1:
+        row.append(1)
+    else: row.append(0)
+    L_j.append(row)
+
+'''Prefrence data reading'''
+# CP1_id: ith crew member prefers dth day to work/off
+CP1_id = [[]]
+for i in range(1,m+1):
+    row = [[]]
+    for d in range(1,dn+1):
+        row.append(int(CP1.cell_value(i,d)))
+    CP1_id.append(row)
+
+# CP_il: ith crew member prefers lth lay-over
+CP2_il = [[]]
+for i in range(1,m+1):
+    row = [[]]
+    for l in range(1,ln+1):
+        row.append(int(CP2.cell_value(i,l)))
+    CP2_il.append(row)
+
+'''
+CP3 = Data_File.sheet_by_index(6)
+CP3_i = [[]]
+CP3_c = [[]] 
+for i in range(1,5):
+    row = [[]]
+    for j in range(1,4):
+        row.append(CP3.cell_value(i,j))
+    CP3_i.append(row)
+
+for j in range(1, 4):
+    col = [[]]
+    for i in range(1,5):
+        if CP3_i[i][j] != '-':
+            col.append(i)
+    CP3_c.append(col)
+
+CP3_leq = [[]]
+if len(CP3_c[1]):
+    for i in range(CP3_c[1][1], CP3_c[1][1]+len(CP3_c[1])-1):
+        CP3_leq.append(CP3.cell_value(i,1))
+else 
+
+CP3_eql = [[]]
+if len(CP3_c[2]):
+    for i in range(CP3_c[2][1], CP3_c[2][1]+len(CP3_c[2])-1):
+        CP3_eql.append(CP3.cell_value(i,2))
+
+CP3_geq = [[]]
+if len(CP3_c[3]):
+    for i in range(CP3_c[3][1], CP3_c[3][1]+len(CP3_c[3])-1):
+        CP3_geq.append(CP3.cell_value(i,3))
+
+CP4 = Data_File.sheet_by_index(7)
+CP4_i = [[]]
+CP4_c = [[]] 
+for i in range(1,5):
+    row = [[]]
+    for j in range(1,4):
+        row.append(CP4.cell_value(i,j))
+    CP4_i.append(row)
+
+for j in range(1, 4):
+    col = [[]]
+    for i in range(1,5):
+        if CP4_i[i][j] != '-':
+            col.append(i)
+    CP4_c.append(col)
+
+CP4_leq = [[]]
+if len(CP4_c[1]):
+    for i in range(CP4_c[1][1], CP4_c[1][1]+len(CP4_c[1])-1):
+        CP4_leq.append(CP4.cell_value(i,1))
+
+CP4_eql = [[]]
+if len(CP4_c[2]):
+    for i in range(CP4_c[2][1], CP4_c[2][1]+len(CP4_c[2])-1):
+        CP4_eql.append(CP4.cell_value(i,2))
+
+CP4_geq = [[]]
+if len(CP4_c[3]):
+    for i in range(CP4_c[3][1], CP4_c[3][1]+len(CP4_c[3])-1):
+        CP4_geq.append(CP4.cell_value(i,3))
+'''
+
+
---
+++ Model.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+import pyomo.environ as pe
+import pyomo.opt
+from Data import * 
+
+model= pe.ConcreteModel()
+
+'''Define parameters, sets & indices, and variables'''
+# Global Parameters
+model.bh = BH_j
+model.lh = LH_j
+model.do = DO_jd
+model.lo = LO_jl
+model.ln = LN_j
+model.ld = LD_j
+model.ol = O_gh
+model.l = L_j
+
+# Preference Parameters
+
+
+# Sets and indices
+model.i = pe.Set(initialize = set(range(1,m+1)))
+model.j = pe.Set(initialize = set(range(1,n+1)))
+model.l = pe.Set(initialize = set(range(1,ln+1)))
+model.d = pe.Set(initialize = set(range(1,dn+1)))
+
+# Variables
+model.x = pe.Var(model.i, model.j, within = pe.Binary)
+model.s1 = pe.Var(model.i, within = pe.NonNegativeReals)
+model.s2 = pe.Var(model.i, within = pe.NonNegativeReals)
+
+'''Objective function'''
+
+i: crew member
+j: pairing CP
+d: day of the month[1:31]
+l: layover ID
+i--m
+j--n
+d--dn
+l--ln
+#### for all i (n)
+
+''' Global Constraints'''
+# block hour - hard
+def block_hard_rule(model, i):
+    return sum ((model.bh[j] * model.x[i,j]) for j in range(1,n+1)) <= 100
+model.block_hard_rule = pe.Constraint(model.i, rule = block_hard_rule)
+
+# block hour - soft
+def block_soft_upper(model, i):
+    return sum ((model.bh[j] * model.x[i,j]) for j in range(1,n+1)) <= 95 + model.s2[i]
+model.block_soft_upper = pe.Constraint(model.i, rule = block_soft_upper)
+
+def block_soft_lower(model, i):
+    return sum ((model.bh[j] * model.x[i,j]) for j in range(1,n+1)) >= 85 - model.s1[i]
+model.block_soft_lower = pe.Constraint(model.i, rule = block_soft_lower)
+
+# Min Days off in a planning period (Max Work)
+def mindayoff_rule(model, i):
+    return sum((sum ((model.do[i][j] * model.x[i,j]) for j in range(1,n+1))) for d in range(1,dn+1)) <= 23
+model.mindayoff_rule = pe.Constraint(model.i, rule = mindayoff_rule)
+
+# Max pairings with layover in a planning period
+def maxlayoverpairing_rule(model, i):
+    return sum ((model.l[j] * model.x[i,j]) for j in range(1,n+1)) <= 4
+model.maxlayoverpairing_rule = pe.Constraint(model.i, rule = maxlayoverpairing_rule)
+
+#### for all j (m)
+#GC- we assume now that one pairing can only assigned to one crewmember
+def onetoone_rule(model, j):
+    return sum((model.x[i,j]) for i in range(1,m+1)) <= 1
+model.onetoone_rule = pe.Constraint(model.j, rule = onetoone_rule)
+
+#### overlap
+def overlap_rule(model, j):
+    for
+    if    : 
+    	return sum((model.x[i,j]) for i in range(1,n+1)) <= 1
+
+model.overlap_rule = pe.Constraint(model.j, rule = overlap_rule)
